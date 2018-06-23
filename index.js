cb7a7aa26102053e13003fe76fd0f77d9f5bd124
Joseph Handley <handleyj@hotmail.com>
2018-06-23 15:32:31 -0500

    rc1

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ var currentNodeVersion = process.versions.node;
 var semver = currentNodeVersion.split(".");
 var major = semver[0];
 
-if (major < 6) {
+if (major < 8) {
     console.error(chalk.red(`You are running Node ${currentNodeVersion}.`));
-    console.error(chalk.red("Create React WP Theme requires Node 6 or higher."));
+    console.error(chalk.red("Create React WP Theme requires Node 8 or higher."));
     console.error(chalk.red("Please update your version of Node."));
     process.exit(1);
 }
