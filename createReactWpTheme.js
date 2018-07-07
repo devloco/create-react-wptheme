4597e1b3ed5c88e78decc6c1599027a207326a2c
Joseph Handley <handleyj@hotmail.com>
2018-07-06 19:57:25 -0500

    bump version

--- createReactWpTheme.js
+++ createReactWpTheme.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018-present devloco
+ * Copyright (c) 2018-present, https://github.com/devloco
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
